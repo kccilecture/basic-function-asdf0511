f3038ed198a042882ded05b5c46bc85163cc61f9
Mino <alsghrkd0511@gmail.com>
2026-03-18 12:04:06 +0900

    수업 자료 바꿈

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,32 @@
 def add(a, b):
-    pass
+      ## pass부분을 코드로 넣어서 pytest로 확인하기
+    return a + b
 
 
 def sub(a, b):
-    pass
+    return a - b
 
 
 def mul(a, b):
-    pass
+    return a * b
 
 
 def div(a, b):
-    pass
+    return a / b
 
 
 def power(base, pow):
-    pass
+    return base**pow
 
 
 def square(base):
-    pass
+    return power(base,2)
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    if 나이 >= 50:
+        return str (f"안녕하십니까 {이름}!")
+    elif 나이 <= 4:
+        return str (f"안녕 {이름}!")
+    else:
+        return str (f"안녕하신가 {이름}!")
